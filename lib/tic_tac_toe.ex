ee4dad5a80e0284417c4259772406236016d7aaf
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-03-26 16:38:26 -0500

    update initial project layout

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -1,18 +1,5 @@
 defmodule TicTacToe do
-  @moduledoc """
-  Documentation for TicTacToe.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TicTacToe.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+    def main() do
+      IO.puts "X/O Game."
+    end
 end
